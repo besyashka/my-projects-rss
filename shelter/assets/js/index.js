c1acf0160586b058eb16b12bbba88ba20639c8ed
Ekaterina <komsa_9191@mail.ru>
2024-08-23 15:22:51 +1000

    feat: add function for load data from json

--- shelter/assets/js/index.js
+++ shelter/assets/js/index.js
@@ -1,6 +1,32 @@
 console.log('100 баллов');
 import { handleClickBurger } from './burger.js';
-import { slider } from './slider.js';
+import { Card } from './Card.js';
+
+let cardsArray = [];
+
+const fetchCard = () => {
+  fetch ('./assets/pets.json')
+  .then((response) => {
+    return response.json();
+  })
+  .then((pets) => {
+    cardsArray = pets.map(({
+      name,
+      img,
+      type,
+      breed,
+      description,
+      age,
+      inoculations,
+      diseases,
+      parasites
+    }) =>
+      new Card(name, img, type, breed, description, age, inoculations, diseases, parasites)
+    );
+
+    getArrayCards(cardsArray)
+  });
+};
 
 handleClickBurger();
-slider();
+fetchCard();
