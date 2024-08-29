8953b610bf39b4f86b11aacd73bce16b99ef2d5d
Ekaterina <komsa_9191@mail.ru>
2024-08-29 13:36:48 +1000

    feat: add a method for the Card class that generate modal window and add style for modal window

--- shelter/assets/js/Card.js
+++ shelter/assets/js/Card.js
@@ -24,4 +24,60 @@ export class Card {
 
     return templateSlide;
   }
+
+  generatePetsCardForModal() {
+    const listItemsArray = [
+      `Age: ${this.age}`,
+      `Inoculations: ${this.inoculations}`,
+      `Diseases: ${this.diseases}`,
+      `Parasite: ${this.parasites}`
+    ];
+
+    const modalWindow = document.createElement('div');
+    const modalContainer = document.createElement('div');
+    const buttonClose = document.createElement('button');;
+    const img = document.createElement('img');
+    const modalContent = document.createElement('div')
+    const nameElement = document.createElement('h3');
+    const typeElement = document.createElement('h4');
+    const descriptionElement = document.createElement('h5');
+    const list = document.createElement('ul');
+
+    modalWindow.classList.add('modal_window');
+    buttonClose.classList.add('button_modal', 'button_control');
+    modalContainer.classList.add('modal_container');
+    img.classList.add('modal_img');
+    modalContent.classList.add('modal_content');
+    descriptionElement.classList.add('modal_text');
+    list.classList.add('modal_list');
+
+    buttonClose.textContent = 'х';
+    img.src = this.img;
+    img.alt = `pets ${this.type}`;
+    nameElement.textContent = this.name;
+    typeElement.textContent = `${this.type} - ${this.breed}`;
+    descriptionElement.textContent = this.description;
+    
+    for (let i = 0; i < listItemsArray.length; i++) {
+      const listItem = document.createElement('li');
+      listItem.textContent = listItemsArray[i];
+      listItem.classList.add('modal_item');
+      list.appendChild(listItem);
+    }
+
+    modalWindow.appendChild(buttonClose);
+    modalWindow.appendChild(modalContainer);
+
+    modalContainer.appendChild(img);
+    modalContainer.appendChild(modalContent);
+
+    modalContent.appendChild(nameElement);
+    modalContent.appendChild(typeElement);
+    modalContent.appendChild(descriptionElement);
+    modalContent.appendChild(list);  
+
+    document.querySelector('.overlay').appendChild(modalWindow);
+    
+    return modalWindow;
+  }
 };
