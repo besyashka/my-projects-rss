9818f9f02297a521d0a3c7ca8b6a3779761ff911
Ekaterina <komsa_9191@mail.ru>
2024-09-05 14:45:55 +1000

    feat: add  functions for create slides and click button next slider

--- shelter/assets/js/slider.js
+++ shelter/assets/js/slider.js
@@ -1,20 +1,59 @@
 const sliderLine = document.querySelector('.slider_line');
 
 export const getArrayCards = (cardsArray) => {
-  shuffleArrayCards(cardsArray);
+  if (sliderLine) {
+    shuffleArrayCards(cardsArray);
+  }
 };
 
 const cleanSlider = () => {
   sliderLine.innerHTML = '';
 }
 
-const shuffleArrayCards = (cardsArray) => { 
-  const cardsArrayLength = cardsArray.length;
-  
+const shuffleArrayCards = (slideArray) => { 
+  const cardsArrayLength = slideArray.length;
+
   for (let i = cardsArrayLength - 1; i > 0; i--) {
     let random = Math.floor(Math.random() * (i + 1)); 
-    [cardsArray[i], cardsArray[random]] = [cardsArray[random], cardsArray[i]];
+    [slideArray[i], slideArray[random]] = [slideArray[random], slideArray[i]];
   }
 
-  generateRandomSlide(cardsArray);
+  createNewSlider(slideArray);
 };
+
+let slideArrayCopy = [];
+
+const createNewSlider = (slideArray) => {
+  cleanSlider();
+  let offset = 0;
+  slideArrayCopy = slideArray.slice();
+
+  slideArray.forEach((card) => {
+    const cardGenerate = card.generateSlideCard();
+    cardGenerate.style.left = `${offset * (270 + 90)}px`;
+    sliderLine.appendChild(cardGenerate);
+    offset++;
+  });
+}
+
+const handleClickButtonSliderNext = () => {
+  document.querySelector('.slider_control_next').addEventListener('click', () => {
+    let slides = document.querySelectorAll('.slide');
+
+    slides.forEach((slide, index) => {
+      slide.style.left = `${(index - 1) * (270 + 90)}px`;
+    });
+    
+    if (slides.length <= 3) {
+      shuffleArrayCards(slideArrayCopy);
+    } else {
+      setTimeout(() => {
+        slides[0].remove();
+      }, 1000);
+    }
+  });
+}
+
+if (document.querySelector('.slider_control_next') !== null) {
+  handleClickButtonSliderNext();
+}
