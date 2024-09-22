2a9ade875a89e32da2a7ce616f44ef3bf86470d0
Ekaterina <komsa_9191@mail.ru>
2024-09-23 01:03:59 +1000

    refactor: move the author and the name of the track to the appropriate place

--- audio-player/assets/js/list-music.js
+++ audio-player/assets/js/list-music.js
@@ -1,37 +1,37 @@
 export const listMusic = [
   {
-    name: "Arsenium and Mianna and Heren",
-    author: "Bon Ami",
+    author: "Arsenium and Mianna and Heren",
+    name: "Bon Ami",
     img: "./assets/images/track-covers/Bon-Ami.jpg",
     audio: "./assets/audio/Arsenium_and_Mianna_and_Heren_-_Bon_Ami.mp3"
   },
   {
-    name: "The PanHeads Band",
-    author: "Monster",
+    author: "The PanHeads Band",
+    name: "Monster",
     img: "./assets/images/track-covers/Monster.jpg",
     audio: "./assets/audio/The_PanHeads_Band-Monster.mp3"
   },
   {
-    name: "The PanHeads Band",
-    author: "SHjopot v temnote",
+    author: "The PanHeads Band",
+    name: "SHjopot v temnote",
     img: "./assets/images/track-covers/шопот-в-темноте.jpg",
     audio: "./assets/audio/The_PanHeads_Band-SHjopot_v_temnote.mp3"
   },
   {
-    name: "ROCK_PRIVET",
-    author: "Nas ne dogonyat",
+    author: "ROCK_PRIVET",
+    name: "Nas ne dogonyat",
     img: "./assets/images/track-covers/ROCK-PRIVET.jpg",
     audio: "./assets/audio/ROCK_PRIVET_-_Nas_Ne_Dogonyat.mp3"
   },
   {
-    name: "The PanHeads Band",
-    author: "Я другой",
+    author: "The PanHeads Band",
+    name: "Я другой",
     img: "./assets/images/track-covers/ot-vsego-ystal.jpg",
     audio: "./assets/audio/The PanHeads Band — Я другой.mp3"
   },
   {
-    name: "The PanHeads Band",
-    author: "Poslannaya nebesami",
+    author: "The PanHeads Band",
+    name: "Poslannaya nebesami",
     img: "./assets/images/track-covers/poslanaya-nebesami.jpg",
     audio: "./assets/audio/The_PanHeads_Band_-_Poslannaya_nebesami.mp3"
   }
