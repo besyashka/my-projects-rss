7bc4e0d41a4c54352345b0af34f5ea863567ffb5
Ekaterina <komsa_9191@mail.ru>
2024-09-29 14:31:28 +1000

    feat: add search based on entered text

--- image-gallery/assets/index.js
+++ image-gallery/assets/index.js
@@ -1,15 +1,23 @@
-const url = 'https://api.unsplash.com/search/photos?query=[spring]&per_page=15&orientation=landscape&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo';
+const containerImage = document.querySelector('.container_image_gallery');
+
+let valueInput = 'spring';
 
 async function getData() {
+  let url = `https://api.unsplash.com/search/photos?query=${valueInput}&per_page=15&orientation=landscape&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo`;
   const res = await fetch(url);
   const data = await res.json();
-  //console.log(data, 'json fail');
 
   renderImages(data);
 }
 
+const cleanContainerImage = () => {
+  document.querySelector('.container_image_gallery').innerHTML = '';
+}
+
 const renderImages = (data) => {
   const arr = data.results;
+
+  cleanContainerImage();
   
   arr.map((value) => {
     const imagePath = value.urls.regular;
@@ -18,7 +26,6 @@ const renderImages = (data) => {
 }
 
 const createImg = (imagePath) => {
-  const containerImage = document.querySelector('.container_image_gallery');
   const elementImg = document.createElement('img');
 
   elementImg.classList.add('image_gallery');
@@ -27,4 +34,15 @@ const createImg = (imagePath) => {
   containerImage.append(elementImg);
 }
 
+const getEnteredTextForSearch = () => {
+  const input = document.querySelector('.input-search');
+  const buttonSearch = document.querySelector('.button_search_icon');
+
+  buttonSearch.addEventListener('click', () => {
+    valueInput = input.value;
+    getData();
+  });
+}
+
 getData();
+getEnteredTextForSearch();
