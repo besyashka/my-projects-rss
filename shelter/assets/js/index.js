270d395cafad0edf962080e8104d59c2dc55dce6
Ekaterina Yurina <komsa_9191@mail.ru>
2025-02-18 08:37:04 +1000

    fix: completely change the slider functions and style for it

--- shelter/assets/js/index.js
+++ shelter/assets/js/index.js
@@ -1,7 +1,7 @@
 console.log('100 баллов');
 import { handleClickBurger } from './burger.js';
 import { Card } from './Card.js';
-import { getArrayCards } from './slider.js';
+import { handleClickButtonSliderNext, handleClickButtonPrev } from './slider.js';
 import { openModalWindow } from './modal-window.js';
 
 const fetchCard = () => {
@@ -24,10 +24,11 @@ const fetchCard = () => {
       new Card(name, img, type, breed, description, age, inoculations, diseases, parasites)
     );
 
-    getArrayCards(cardsArray);
     openModalWindow(cardsArray);
   });
 };
 
 handleClickBurger();
 fetchCard();
+handleClickButtonSliderNext();
+handleClickButtonPrev();
--- shelter/assets/js/slider.js
+++ shelter/assets/js/slider.js
@@ -1,99 +1,42 @@
 const sliderLine = document.querySelector('.slider_line');
+let position = 0;
 
-export const getArrayCards = (cardsArray) => {
-  if (sliderLine) {
-    shuffleArrayCards(cardsArray);
-  }
-};
-
-const cleanSlider = () => {
-  sliderLine.innerHTML = '';
-}
-
-const shuffleArrayCards = (slideArray) => { 
-  const cardsArrayLength = slideArray.length;
-
-  for (let i = cardsArrayLength - 1; i > 0; i--) {
-    let random = Math.floor(Math.random() * (i + 1)); 
-    [slideArray[i], slideArray[random]] = [slideArray[random], slideArray[i]];
-  }
-
-  createNewSlider(slideArray);
-};
-
-let slideArrayCopy = [];
-
-const createNewSlider = (slideArray) => {
-  cleanSlider();
-  let offset = 0;
-  slideArrayCopy = slideArray.slice();
-
-  slideArray.forEach((card) => {
-    const cardGenerate = card.generateSlideCard();
-    cardGenerate.style.left = `${offset * (270 + 90)}px`;
-    sliderLine.appendChild(cardGenerate);
-    offset++;
-  });
-}
-
-const handleClickButtonSliderNext = () => {
-  document.querySelector('.slider_control_next').addEventListener('click', () => {
-    let slides = document.querySelectorAll('.slide');
-    let numberOfSlides = checkWidth();
-
-    slides.forEach((slide, index) => {
-      slide.style.left = `${(index - numberOfSlides) * (270 + 90)}px`;
-    });
-
-    setTimeout(() => {
-      for (let i = 0; i < numberOfSlides; i++) {
-        slides[i].remove();
-      }
-
-      slides = document.querySelectorAll('.slide');
-
-      if (slides.length <= 3) {
-        shuffleArrayCards(slideArrayCopy);
-      }
-    }, 1000);
-  });
-}
-
-const handleClickButtonPrev = () => {
-  document.querySelector('.slider_control_prev').addEventListener('click', () => {
-    let slides = document.querySelectorAll('.slide');
-    let numberOfSlides = checkWidth();
-
-    slides.forEach((slide, index) => {
-      slide.style.left = `${(index + numberOfSlides) * (270 + 90)}px`;
-    });
-
-    setTimeout(() => {
-      /* for (let i = 0; i < numberOfSlides; i++) {
-        slides[i].remove();
-      } */
-
-      shuffleArrayCards(slideArrayCopy);
-    }, 1000);
-  });
-}
-
-const checkWidth = () => {
-  let number = null;
-
-  if (window.innerWidth < 708) {
-    number = 1;
-  } else if (window.innerWidth < 1201) {
-    number = 2;
+const getSliderGap = () => {
+  if (window.innerWidth < 1201) {
+    return 40;
   } else {
-    number = 3;
+    return 90;
   }
+};
 
-  return number;
-}
+let gap = getSliderGap();
 
-if (document.querySelector('.slider_control_next') !== null) {
-  handleClickButtonSliderNext();
-  handleClickButtonPrev();
-  window.addEventListener('resize', checkWidth);
-}
+export const handleClickButtonSliderNext = () => {
+  document.querySelector('.slider_control_next').addEventListener('click', () => {
+    position += (270 + gap);
+
+    if (position > 1980) {
+      position = 0;
+    }
+
+    sliderLine.style.transform = `translateX(${-position}px)`;
+  });
+};
+
+export const handleClickButtonPrev = () => {
+  document.querySelector('.slider_control_prev').addEventListener('click', () => {
+    position -= (270 + gap);
+
+    if (position < 0) {
+      position = 1890 - gap;
+    }
+
+    sliderLine.style.transform = `translateX(${-position}px)`;
+  });
+};
+
+window.addEventListener('resize', () => {
+  gap = getSliderGap();
+  sliderLine.style.transform = `translateX(0px)`;
+  console.log(gap)
+});
